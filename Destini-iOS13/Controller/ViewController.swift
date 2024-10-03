c3c32530917de09fcf4359a26822ac346707559c
Vika <vg.dev31@gmail.com>
2024-10-03 13:57:45 +0400

    Story app with small array (first version)

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var storyBrain = StoryBrain()
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        storyLabel.text = storyBrain.getStoryLabel()
+        choice1Button.setTitle(storyBrain.getChoice1Label(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2Label(), for: .normal)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(sender.currentTitle!)
+        updateUI()
+    }
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,42 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    let story = [Story(title: "You see a fork in the road",
+                        choice1: "Take a left",
+                        choice2: "Take a right"),
+                 Story(title: "You see a tiger",
+                       choice1: "Shout for help",
+                       choice2: "Play dead"),
+                 Story(title: "You find a treasure chest",
+                       choice1: "Open it",
+                       choice2: "Check for traps"),
+    ]
+    
+    var storyNumber: Int = 0
+    
+    func getStoryLabel() -> String {
+        return story[storyNumber].title
+    }
+    
+    func getChoice1Label() -> String {
+        return story[storyNumber].choice1
+    }
+    
+    func getChoice2Label() -> String {
+        return story[storyNumber].choice2
+    }
+    
+    mutating func nextStory(_ userChoice: String) {
+        
+        if userChoice == story[storyNumber].choice1 {
+            storyNumber += 1
+        } else if userChoice == story[storyNumber].choice2 {
+            storyNumber += 2
+        }
+        
+        if storyNumber >= story.count {
+                    storyNumber = 0 // Можно сбросить индекс, чтобы начать сначала
+                }
+    }
+}
